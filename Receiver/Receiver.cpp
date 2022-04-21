b716b87c5bf3feabd809cf484c36dbea5e91eefb
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-21 18:36:16 +0530

    Update Receiver.cpp

--- Receiver/Receiver.cpp
+++ Receiver/Receiver.cpp
@@ -1,1 +1,45 @@
+#include "Receiver.h"
 
+bool tclReceiver::bReadConsoleOutputFromFileIntoVector(vector<stBatteryValues> &m_BatteryValues)
+{
+   bool bReturnVal = false;
+   ifstream indata;
+   indata.open("./Receiver/consoleOutput.txt");
+   if (!indata)
+   {
+      cout <<" \n File read failed";
+      bReturnVal = false;
+   }
+   else
+   {
+      std::string line;
+      while (std::getline(indata, line))
+      {
+         stringstream oLineStream(line);
+         std::string IndividualWord;
+         int digitcount = 1;
+         stBatteryValues oBatteryValues;
+         while (oLineStream >> IndividualWord)
+         {
+            float number;  //extract temperature and ChargeRate value.
+            if (stringstream(IndividualWord) >> number)
+            {
+               if (digitcount == 1)
+               {
+                  // first word is a temperature
+                  oBatteryValues.temperature = number;
+               }
+               else
+               {
+                  oBatteryValues.chargeRate = number;
+               }
+               ++digitcount;
+            }
+         }
+         m_BatteryValues.push_back(oBatteryValues);
+      }
+      indata.close();
+      bReturnVal = true;
+   }
+   return bReturnVal;
+}
