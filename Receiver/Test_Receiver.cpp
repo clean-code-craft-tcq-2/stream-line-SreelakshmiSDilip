a12d46835d85101b6de4a9d1dcfb9ce085dbd10f
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-21 18:16:03 +0530

    Update Test_Receiver.cpp

--- Receiver/Test_Receiver.cpp
+++ Receiver/Test_Receiver.cpp
@@ -7,6 +7,6 @@ TEST_CASE("Test To check the retrieve of console ouput values")
    tclReceiver oReceiver;
    vector<stBatteryValues> oBatteryValues;
    bool bSuccess = oReceiver.bReadConsoleOutputFromFileIntoVector(oBatteryValues);
-   assert(bSuccess == true)
+   assert(bSuccess == true);
 }
 
