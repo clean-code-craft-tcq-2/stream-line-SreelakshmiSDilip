56902e4f8174fb4d71d9bcc4aaf460e5e3560db5
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-21 17:16:51 +0530

    Update Receiver.h

--- Receiver/Receiver.h
+++ Receiver/Receiver.h
@@ -1,1 +1,42 @@
+/*
+   This file holds the declaration of Receiver class.
+   The input to this receiver is in the below form of floats (from Sender Console Output)
+   Temperature value == 24.000000   ChargeRate value == 60.000000
+*/
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include<vector>
+using namespace std;
+
+struct stBatteryValues
+{
+   float temperature;
+   float chargeRate;
+};
+
+static void vDisplayBatteryValues(const vector<stBatteryValues> &m_BatteryValues)
+{
+   for (int index = 0; index < m_BatteryValues.size(); ++index)
+   {
+      cout << m_BatteryValues[index].temperature << "\t" << m_BatteryValues[index].chargeRate << "\n";
+   }
+}
+
+class tclReceiver    //tcl -> type class
+{
+
+public:
+
+   /*
+   Notes:
+   Reads console output from Sender. Currently the output of console is assumed to be stored in a file
+   named consoleOutput.txt
+   */
+   bool bReadConsoleOutputFromFileIntoVector(vector<stBatteryValues> &m_BatteryValues);
+};
 
