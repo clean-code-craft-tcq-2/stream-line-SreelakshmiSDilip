980c5bea60d58c276fd7b13a9e7d743a628dc6ee
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-23 15:07:17 +0530

    Update Test_Receiver.cpp

--- Receiver/Test_Receiver.cpp
+++ Receiver/Test_Receiver.cpp
@@ -9,3 +9,17 @@ TEST_CASE("Test To check the retrieve of console ouput values")
    assert(!oReceiver.vGetBatteryValues().empty() == true);
    vDisplayBatteryValues(oReceiver.vGetBatteryValues());
 }
+
+TEST_CASE("Test To check the correctness of min value calculation of Battery temperature") 
+{
+   tclReceiver oReceiver;
+   bool bSuccess = oReceiver.bReadConsoleOutputFromFileIntoVector();
+   assert(bSuccess == true);
+   assert(!oReceiver.vGetBatteryValues().empty() == true);
+   
+   // test minimum value.
+   float fExpectedMinTempValue = 10;
+   float fCalculatedMinTempValue= oReceiver.fGetMinOfBatteryParamterFromSenderStream(teBatteryParameter::EN_TEMPERATURE);
+   assert(fExpectedMinTempValue == fCalculatedMinTempValue);
+}
+
