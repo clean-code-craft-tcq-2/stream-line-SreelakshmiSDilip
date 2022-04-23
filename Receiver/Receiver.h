f16e0d44b8c25d4be53634be813e20965e96afa3
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-23 14:27:18 +0530

    Update Receiver.h

--- Receiver/Receiver.h
+++ Receiver/Receiver.h
@@ -1,8 +1,8 @@
 /*
-   This file holds the declaration of Receiver class.
-   The input to this receiver is in the below form of floats (from Sender Console Output)
-   Temperature value == 24.000000   ChargeRate value == 60.000000
-   Currently the output of console is assumed to be stored in a file named consoleOutput.txt
+This file holds the declaration of Receiver class.
+The input to this receiver is in the below form of floats (from Sender Console Output)
+Temperature value == 24.000000   ChargeRate value == 60.000000
+Currently the output of console is assumed to be stored in a file named consoleOutput.txt
 */
 
 #pragma once
@@ -14,6 +14,12 @@
 #include<vector>
 using namespace std;
 
+enum teBatteryParameter
+{
+   EN_TEMPERATURE = 0,
+   EN_CHARGERATE
+};
+
 struct stBatteryValues
 {
    float temperature;
@@ -22,9 +28,9 @@ struct stBatteryValues
 
 static void vDisplayBatteryValues(const vector<stBatteryValues> &m_BatteryValues)
 {
-   cout <<"\n\n******Received Values From Sender******\n";
-   
-   for (int index = 0; index < (int) m_BatteryValues.size(); ++index)
+   cout << "\n\n******Received Values From Sender******\n";
+
+   for (int index = 0; index < (int)m_BatteryValues.size(); ++index)
    {
       cout << m_BatteryValues[index].temperature << "\t" << m_BatteryValues[index].chargeRate << "\n";
    }
@@ -32,14 +38,20 @@ static void vDisplayBatteryValues(const vector<stBatteryValues> &m_BatteryValues
 
 class tclReceiver    //tcl -> type class
 {
-
 public:
+
+   vector<stBatteryValues> vGetBatteryValues();
 
    /*
    Notes:
    Reads console output from Sender. Currently the output of console is assumed to be stored in a file
    named consoleOutput.txt
    */
-   bool bReadConsoleOutputFromFileIntoVector(vector<stBatteryValues> &m_BatteryValues);
-};
+   bool bReadConsoleOutputFromFileIntoVector();
+   float fGetMinOfBatteryParamterFromSenderStream(teBatteryParameter oBatteryValue);
+   float fGetMaxOfBatteryParamterFromSenderStream(teBatteryParameter oBatteryValue);
+   float fDetermineMovingAverageOfBatteryParamFromSenderStream(teBatteryParameter oBatteryValue);
 
+private:
+   vector<stBatteryValues> m_BatteryValues;
+};
